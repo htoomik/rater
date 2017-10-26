ef72d0bdc343f678dc99277ad8e940c36ce31d21
Helen Toomik <helen@toomik.net>
2017-10-26 23:15:14 +0200

    refactor: use Dictionary for storage
    … so we can find existing items more easily

--- Rater.Api/Data/SkillsDataStore.cs
+++ Rater.Api/Data/SkillsDataStore.cs
@@ -5,28 +5,30 @@ namespace Rater.Api.Data
 {
     public class SkillsDataStore : ISkillsDataStore
     {
-        private readonly List<Skill> skills = new List<Skill>();
+        private readonly Dictionary<int, Skill> skills = new Dictionary<int, Skill>();
 
         public int Count => skills.Count;
 
 
         public Skill Add(Skill value)
         {
-            skills.Add(value);
-            value.Id = skills.Count;
+            value.Id = skills.Count + 1;
+            skills.Add(value.Id, value);
             return value;
         }
 
 
         public List<Skill> Get()
         {
-            return new List<Skill>(skills);
+            return new List<Skill>(skills.Values);
         }
 
 
         public Skill Get(int id)
         {
-            return skills.SingleOrDefault(s => s.Id == id);
+            if (!skills.ContainsKey(id))
+                return null;
+            return skills[id];
         }
     }
 }
