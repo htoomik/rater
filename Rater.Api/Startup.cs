cf849805887dd637c31156fe9a8d657fc1a039fe
Helen Toomik <helen@toomik.net>
2017-10-28 13:05:21 +0200

    chore: Angularize!
    - Add Angular and all its dependencies
    - Hook them all up
    - Adjust Startup.cs to server node_modules to browser

--- Rater.Api/Startup.cs
+++ Rater.Api/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Rater.Api.Data;
@@ -37,6 +40,12 @@ namespace Rater.Api
             }
 
             app.UseMvc();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/node_modules")
+            });
         }
     }
 }
