dea2fb80cbe3eefa0f974291bd966efcaefd0553
Helen Toomik <helen@toomik.net>
2017-10-28 13:52:00 +0200

    feat: list skills
    List skills. Currently all hard-coded data and no styling.

--- Rater.Api/wwwroot/app/app.component.ts
+++ Rater.Api/wwwroot/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
 
+export class Skill
+{
+    id: number;
+    name: string;
+    rating: number;
+}
+
+const SKILLS : Skill[] =
+[
+    { id: 1, name: ".NET", rating: 5 },
+    { id: 2, name: "SQL", rating: 4 },
+    { id: 3, name: "JavaScript", rating: 2 },
+    { id: 4, name: "TypeScript", rating: 1 },
+    { id: 5, name: "Angular", rating: 1 },
+]
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <ul>
+        <li *ngFor="let skill of skills">
+            <span>{{skill.name}}</span>
+            <span>{{skill.rating}}</span>
+        </li>
+    </ul>
+    `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent
+{
+    title = 'Skills';
+    skills = SKILLS;
+}
